9be05ce6880de66f370db156ff8c34d418aa6085
galaxy_ms(SENS) <galaxy_ms@naver.com>
2017-04-11 15:51:39 +0900

    14499 Accept

--- acmicpc/Accept/14499.c
+++ acmicpc/Accept/14499.c
@@ -6,7 +6,7 @@
 
 int dice[7];
 int rowSize, colSize, map[22][22];
-int dyx[5][2] = { {0, 0}, {0, 1}, {0, -1}, {-1, 0}, {1, 0} };
+int dyx[5][2] = { { 0, 0 },{ 0, 1 },{ 0, -1 },{ -1, 0 },{ 1, 0 } };
 int diceRow, diceCol, cmdCount;
 
 int main()
