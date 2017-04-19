b95b6926ea0b545f514931ef86df017a15bfc3a9
galaxy_ms(SENS) <galaxy_ms@naver.com>
2017-04-19 17:53:04 +0900

    push

--- acmicpc/algo.c
+++ acmicpc/algo.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 
+int n, arr[222];
+
+void quicksort(int low, int high)
+{
+	int mid, left, right, t;
+
+	if (low < high)
+	{
+		mid = arr[(low + high) / 2];
+		left = low - 1;
+		right = high + 1;
+		while (1)
+		{
+			do { left++; } while (arr[left] < mid);
+			do { right--; } while (arr[right] > mid);
+			if (left >= right) break;
+			t = arr[left];
+			arr[left] = arr[right];
+			arr[right] = t;
+		}
+		quicksort(low, left - 1);
+		quicksort(right + 1, high);
+	}
+}
+
 int main()
 {
+	int i;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+		scanf("%d", &arr[i]);
+
+	quicksort(0, n - 1);
+
+	for (i = 0; i < n; i++)
+		printf("%4d", arr[i]);
+
 	return 0;
 }
